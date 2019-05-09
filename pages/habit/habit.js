431a43ee933ee1941626f6c380be9b07166c0412
liuyuqing <3533615338@qq.com>
2019-05-09 19:45:08 +0800

    修改习惯列表界面
    添加习惯列表并且添加了weui.wxss

--- pages/habit/habit.js
+++ pages/habit/habit.js
@@ -1,4 +1,5 @@
 // pages/habit/habit.js
+var base64 = require("../images/base64");
 Page({
 
   /**
@@ -12,7 +13,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      icon: base64.icon20
+    });
   },
 
   /**
