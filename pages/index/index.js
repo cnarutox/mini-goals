cf98c7e1af455ffca18164c984268c74714d1036
陈戌 <1612839@mail.nankai.edu.cn>
2019-05-25 09:22:19 +0800

    完成登录功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -84,23 +84,6 @@ Page({
     },
 
     onLoad: function () {
-      wx.login({
-        success(res) {
-          console.log(21)
-          if (res.code) {
-            // 发起网络请求
-            console.log(res.code)
-            // wx.request({
-            //   url: 'https://test.com/onLogin',
-            //   data: {
-            //     code: res.code
-            //   }
-            // })
-          } else {
-            console.log('登录失败！' + res.errMsg)
-          }
-        }
-      })
         this.load();
     },
 
