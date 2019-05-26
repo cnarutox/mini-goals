d71e77fb6a29032723a06e626ee2972dd1f26f32
cnarutox <cnarutox@mail.nankai.edu.cn>
2019-05-26 10:51:20 +0800

    在朋友页面实现初步框架

--- pages/friends/friends.js
+++ pages/friends/friends.js
@@ -5,7 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    thumbupicon: "../images/zan.png",
+    likeicon: "../images/like.png",
+    friendArray:[
+      {
+        userName: 'Flora',
+        icon: 'https://wx.qlogo.cn/mmopen/vi_32/CdhiaPlII2pp1AmfhVjcxkzl6zInhUIiayFx2iabKmRX2mSIoc8pQa2rdwpLCT4C86rrCx0y7z0oczGoJ7f7cIsWQ/132',
+        time: '4分钟前',
+        habitcolor: 'black',
+        habitName: '洗澡澡',
+        habitpersist: 9,
+        likelist: ['张三','李四','王五','陈戌']
+      },
+      {
+        userName: 'cnarutox',
+        icon: 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83er7mP5wibqzZ4icO7VDkId75AiaOCEbSVYuqM8eZVIXPsMSN998MHZq6cpLbMaOZ56WPKOTCIPMcF0Bg/132',
+        time: '昨天',
+        habitcolor: 'yellow',
+        habitName: '吃早饭',
+        habitpersist: 10,
+        likelist: ['1', '2', '3', '4', '1', '2', '3', '4']
+      }, 
+      {
+        userName: 'Flora',
+        icon: 'https://wx.qlogo.cn/mmopen/vi_32/CdhiaPlII2pp1AmfhVjcxkzl6zInhUIiayFx2iabKmRX2mSIoc8pQa2rdwpLCT4C86rrCx0y7z0oczGoJ7f7cIsWQ/132',
+        time: '2019-5-10',
+        habitcolor: 'green',
+        habitName: '爱万戌哥',
+        habitpersist: 40,
+        likelist: ['1', '2', '3', '4']
+      },
+    ]
   },
 
   /**
