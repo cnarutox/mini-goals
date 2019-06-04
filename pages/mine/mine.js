facc5579d91d14301f3526134457173ab2f6ccae
liuxi <oncwnuMEuTQ2k3Z1xoYoxTgNHwek@git.weixin.qq.com>
2019-06-04 20:35:51 +0800

    完善登录功能

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo: {},
+    userInfo: app.globalData.userInfo,
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     likenum: 0,
@@ -18,9 +18,10 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function () {
+  onLoad: function() {
     var that = this
     if (app.globalData.userInfo) {
+      console.log(9999999)
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -28,7 +29,7 @@ Page({
     }
     wx.request({
       url: getApp().globalData.serverUrl + '/api/habit/user-like?openid=' + app.globalData.userInfo.id,
-      success: function (res) {
+      success: function(res) {
         console.log(res.data); // 服务器回包信息
         that.setData({
           likenum: res.data
@@ -40,60 +41,60 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    this.onLoad();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
-  navigatetomypage: function () {
+  navigatetomypage: function() {
     console.log('navigatetomypage')
     wx.navigateTo({
       url: '../mypage/mypage'
     })
   },
 
-  navigatetototalpersist: function () {
+  navigatetototalpersist: function() {
     console.log('avigatetototalpersist')
     wx.navigateTo({
       url: '../totalpersist/totalpersist'
